232eb1379b2e74c6469e884c7c32fe757cf296bc
silva <chenghongliang@cmos.chinamobile.com>
2018-09-29 18:52:38 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
+// import { AjaxPlugin } from 'vux'
+import vConsole from 'vconsole'
+Vue.use(VueRouter)
+// console.log(AjaxPlugin)
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+FastClick.attach(document.body)
+// console.log(AjaxPlugin)
+Vue.config.productionTip = false
+// Vue.http.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
