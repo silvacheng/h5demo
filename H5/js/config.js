81675b87419815dacc04cc46c4e7424bdb6b1415
silva <chenghongliang@cmos.chinamobile.com>
2019-05-14 17:20:25 +0800

    添加 测试代码

---
+++ H5/js/config.js
@@ -0,0 +1,16 @@
+window.configData = {
+    "sdkappid": 1400201097,
+    "users": [{
+        "userId": "Web_trtc_01",
+        "userToken": "eJxlj0tPg0AUhff8CsLa6Azl6a4WWikV*kpa3UwoM*i1FsaZsUAb-7uKGkm82*-LOeeeNV3XjfVsdZnlefVWKqJazgz9WjeQcfEHOQdKMkUGgv6DrOEgGMkKxUQHsW3bJkJ9BygrFRTwY2zYjiihcoJwT5J0T7qm7xQLIRNh5Lt9BR47eBfej6JFsNxEc4ljujhV9e55O3LKegb*WImrtql5GPPGC6UHk2pcR0-D1JxMg5V-I63stpm2cbTcv8TxARf*KXHWaeA*JK-D43ae8LRXqeDAft*yPM91XKdHj0xIqMpO*FxrY3OAvs7Q3rUP8fNftw__"
+    }, {
+        "userId": "Web_trtc_02",
+        "userToken": "eJxlj1FPgzAUhd-5FYRXjLaFDjTxASYLuKlsbOp4aVgprA6h6aoyjf9dRY1NvK-fl3POfTNM07SWs*y4oLR7ahVRB8Es88y0gHX0B4XgJSkUcWT5D7JecMlIUSkmBwgxxggA3eElaxWv*I9xxzZESUUJQJq0L3dkaPpOcQFAAIJTT1d4PcCraDVOgkOZoEmyna5RHzuyDtM8u269NKwXffc6lxGON64zecjndsCjwF1dbNEC2ny9vG-86e4yZXiWivHLbZHRDOZx1ZyE9k1Em*Rcq1T8kf2*5fq*N-JGGn1mcs*7dhA*12KIHPB1lvFufAAFQ129"
+    }, {
+        "userId": "Web_trtc_03",
+        "userToken": "eJxlj1FPgzAUhd-5FYTXGXtpV2Ame2DLFI2bTsjmW1OhYGWjpHQ4Yvzvm6iRxPv6fTnn3A-Ltm0nuY8veZqqQ2WY6Wrh2Fe2A87FH6xrmTFuGNHZPyiOtdSC8dwI3UOXUooBho7MRGVkLn*MrXhhRpuUARlITVayvuk7ZQyAwYWJP1Rk0cPlYj2-XaCb1aZbRXsdVQ84piW6E2-tUs1UCylZR2FOozhBWyXjUIYpabDr*TvetLB7VMH1BGbJJnh970bhCHH9fHiCYl5wVIbT6aDSyL34fWscBL7newPaCt1IVfXCeS11MYGvc6xP6wQIO12-"
+    }, {
+        "userId": "Web_trtc_04",
+        "userToken": "eJxlj1FPwjAUhd-3K5a9YrTdWoYmPuBALWzIJjjhpZlbqVdga0pHBON-V6fGJd7X78s5575Ztm07s-D*NMvzqi4NNwclHPvCdpBz8geVgoJnhnu6*AfFqwIteLYyQjcQU0pdhNoOFKI0sIIfIxVP3GiTc0Ra0q5Y86bpO4Ug5CKMzv22ArKB0XAesDjIrxIZVXFwN4CH9TS-ParHIiR0loxHkpQpMuQZziiTm6lkkh3S65vJcjjpR516UC4CsxH1to-x-Ji8jBZ4qTyIoSPHIbtsVRrYit*3SK-nd-1ui*6F3kFVNsLnWopdD32dY71bH3aNXr8_"
+    }]
+}
